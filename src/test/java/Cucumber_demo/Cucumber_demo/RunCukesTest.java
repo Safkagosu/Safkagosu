663926a8a27c080c522a42cc63220729aa9b7021
Safkagosu <51691946+Safkagosu@users.noreply.github.com>
2020-03-19 16:04:52 +0300

    Update RunCukesTest.java

--- src/test/java/Cucumber_demo/Cucumber_demo/RunCukesTest.java
+++ src/test/java/Cucumber_demo/Cucumber_demo/RunCukesTest.java
@@ -11,7 +11,8 @@ import cucumber.api.junit.Cucumber;
 		monochrome = true, 
 		features = { "classpath:resourses/feature" }, 
 		glue = { "classpath:Cucumber_demo/Cucumber_demo" },
-		tags = { "@run" })
+		tags = { "@run", "@SI_test" }
+		)
 	public class RunCukesTest {
 	}
 
